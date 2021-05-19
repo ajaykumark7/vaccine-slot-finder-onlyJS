c5d553cf4c06517e79591d8ee3dad1baabd23384
h20170039 <46812748+h20170039@users.noreply.github.com>
2021-05-19 13:16:33 +0530

    performed code formatting

--- scripts/default.js
+++ scripts/default.js
@@ -83,42 +83,41 @@ function getFormattedDateHelper(inputDay) {
 }
 
 // 4. fetch availability details and send OTP if vaccine stock available
-function fetchAvailabilityDetailsAndAlertUser(districtId,numberOfWeeks,phone) {
-    
+function fetchAvailabilityDetailsAndAlertUser(districtId, numberOfWeeks, phone) {
+
   //clear the output window once a new search is triggered
   document.querySelector('#output-div').innerHTML = '';
 
   //check vaccine availability for each week
-  for(let week = 0;week < numberOfWeeks;week++) {
+  for (let week = 0; week < numberOfWeeks; week++) {
     let startDate = getFormattedDateHelper(week * 7);
     const endpoint = 'https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/calendarByDistrict?district_id=' + districtId + '&date=' + startDate;
     fetch(endpoint)
       .then(response => response.json())
-      .then(function(response) {
-        console.log(response['centers'].length);
+      .then(function (response) {
         //filter the response by age groups
         function filterByAgeGroup(response) {
           response['centers'] = response['centers'].filter(center => {
-            for(let sessionIndex = 0;sessionIndex < center['sessions'].length;sessionIndex++) {
-              if(center['sessions'][sessionIndex].min_age_limit == document.querySelector('#age').value) {
+            for (let sessionIndex = 0; sessionIndex < center['sessions'].length; sessionIndex++) {
+              if (center['sessions'][sessionIndex].min_age_limit == document.querySelector('#age').value) {
                 return center;
               }
-    
+
             }
           })
           return response['centers'];
         }
 
         //used to optimise performance, ensuring that DOM will be updated only once for each week's data
-        function buildOutputHelper(response,centerIndex,sessionIndex,output) {
+        function buildOutputHelper(response, centerIndex, sessionIndex, output) {
           output = output + '<p>' + response['centers'][centerIndex]['name'] + ' ' + response['centers'][centerIndex]['sessions'][sessionIndex]['date'] + ' ' + response['centers'][centerIndex]['sessions'][sessionIndex]['available_capacity'] + ' ' + response['centers'][centerIndex]['sessions'][sessionIndex]['min_age_limit'] + '</p>';
           return output;
         }
-            
+
         function attachToOutputHelper(output) {
           document.querySelector('#output-div').innerHTML = output;
         }
-    
+
         function notifyViaOTP(phone) {
           let OTPRequestBody = {
             mobile: phone
@@ -136,26 +135,26 @@ function fetchAvailabilityDetailsAndAlertUser(districtId,numberOfWeeks,phone) {
             referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
             body: JSON.stringify(OTPRequestBody)
           })
-            .then(function(response) {
+            .then(function (response) {
               if (!response.ok) {
                 alert(response.statusText + ': OTP already sent');
               }
             })
-    
+
         }
-            
+
         //filter response according to required age range
         response['centers'] = filterByAgeGroup(response);
 
-        if(response['centers'].length > 0) {
+        if (response['centers'].length > 0) {
           //No need to write custom logic to prevent multiple calls of this function(which will happen once per each week's data fetched), since API endpoint is configured to return status 400 for multiple calls in less than 3 minutes
-          if(phone != 0)
+          if (phone != 0)
             notifyViaOTP(phone);
           let output = '<h3>Week ' + (week + 1) + '</h3>';
-          for(let centerIndex = 0;centerIndex < response['centers'].length;centerIndex++) {
-            for(let sessionIndex = 0;sessionIndex < response['centers'][centerIndex]['sessions'].length;sessionIndex++) {
-              if(response['centers'][centerIndex]['sessions'][sessionIndex]['available_capacity'] > 0) {
-                output = buildOutputHelper(response,centerIndex,sessionIndex,output);
+          for (let centerIndex = 0; centerIndex < response['centers'].length; centerIndex++) {
+            for (let sessionIndex = 0; sessionIndex < response['centers'][centerIndex]['sessions'].length; sessionIndex++) {
+              if (response['centers'][centerIndex]['sessions'][sessionIndex]['available_capacity'] > 0) {
+                output = buildOutputHelper(response, centerIndex, sessionIndex, output);
               }
             }
           }
@@ -168,13 +167,13 @@ function fetchAvailabilityDetailsAndAlertUser(districtId,numberOfWeeks,phone) {
 let districts = '';
 districtList.forEach(district => {
   districts += '<option value=' + district.district_id + '>' + district.district_name + '</option>';
-}); 
+});
 document.querySelector('#district-select').innerHTML = districts;
 
 // 2. validate weeks input for -ve values
 const weeks = document.querySelector('#weeks');
-weeks.addEventListener('blur',function() {
-  if(this.value < 1) {
+weeks.addEventListener('blur', function () {
+  if (this.value < 1) {
     alert('Please provide positive value for weeks');
     this.value = 1;
   }
@@ -182,10 +181,10 @@ weeks.addEventListener('blur',function() {
 
 // 3. When 'check availability' Button is clicked, call API to fetch vaccine availability
 const checkAvlBtn = document.querySelector('#check-avl');
-checkAvlBtn.addEventListener('click',function() {
+checkAvlBtn.addEventListener('click', function () {
   let districtId = document.querySelector('#district-select').value;
   let numberOfWeeks = document.querySelector('#weeks').value;
   let phone = document.querySelector('#phone').value;
-  fetchAvailabilityDetailsAndAlertUser(districtId,numberOfWeeks,phone);
+  fetchAvailabilityDetailsAndAlertUser(districtId, numberOfWeeks, phone);
 });
 
